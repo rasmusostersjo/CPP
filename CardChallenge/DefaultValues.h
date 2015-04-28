5e3630f5e0893105067c7b2f7ba0eb150fa4bc3e
Rasmus Östersjö <rasmus.ostersjo@gmail.com>
2015-04-28 21:36:13 +0200

    Removed index-macro

--- CardChallenge/DefaultValues.h
+++ CardChallenge/DefaultValues.h
@@ -3,6 +3,5 @@
 
 #define DEFAULT_DECK_SIZE 52    // [Ace-King] in all four colors
 #define DEFAULT_LV        26    // Let 26 cards be the default lv
-#define ROUND_INDEX_BEG   1     // Let rounds be indexed from 1, 2, ...
 
 #endif // DEFAULT_VALUES_H
