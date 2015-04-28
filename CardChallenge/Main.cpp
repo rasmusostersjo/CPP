e4e48d0c85537aacb96f33d3bd682a50643ed0a6
Rasmus Östersjö <rasmus.ostersjo@gmail.com>
2015-04-28 21:35:45 +0200

    User can now choose lv

--- CardChallenge/Main.cpp
+++ CardChallenge/Main.cpp
@@ -2,9 +2,20 @@
 
 int main(int argc, char *argv[])
 {
-    srand(time(NULL));
-    CardChallenge c(6);
-    c.play();
+    int lv;
+
+    // Let user choose lv
+    std::cout << "Enter lv: ";
+    std::cin  >> lv;
+    
+    // Seed, shuffle and play the card challenge
+    if (lv > 0) {
+        srand(time(NULL));
+        CardChallenge c(lv);
+        c.shuffle().play();
+    }
+    else
+        std::cerr << "Invalid lv; the card challenge closes." << std::endl;
 
     return 0;
 }
--- CardChallenge/Main.h
+++ CardChallenge/Main.h
@@ -4,5 +4,6 @@
 #include "CardChallenge.h"
 #include <cstdlib>          // srand
 #include <ctime>            // time
+#include <iostream>         // cout, cerr
 
 #endif // MAIN_H
