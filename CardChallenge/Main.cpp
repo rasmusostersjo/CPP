f3186e587697b5113fa0704d6d2491526abf49bd
Rasmus Östersjö <rasmus.ostersjo@gmail.com>
2015-04-28 22:19:20 +0200

    read lv as a string

--- CardChallenge/Main.cpp
+++ CardChallenge/Main.cpp
@@ -2,14 +2,15 @@
 
 int main(int argc, char *argv[])
 {
+    std::string strLv;
     int lv;
 
     // Let user choose lv
     std::cout << "Enter lv: ";
-    std::cin  >> lv;
+    std::cin  >> strLv;
     
     // Seed, shuffle and play the card challenge
-    if (lv > 0) {
+    if ((lv = atoi(strLv.c_str())) > 0) {
         srand(time(NULL));
         CardChallenge c(lv);
         c.shuffle().play();
--- CardChallenge/Main.h
+++ CardChallenge/Main.h
@@ -2,8 +2,9 @@
 #define MAIN_H
 
 #include "CardChallenge.h"
-#include <cstdlib>          // srand
+#include <cstdlib>          // srand, atoi
 #include <ctime>            // time
 #include <iostream>         // cout, cerr
+#include <string>           // string
 
 #endif // MAIN_H
