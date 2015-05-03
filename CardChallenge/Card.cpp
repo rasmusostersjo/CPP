6f9b674b190e0ac60b4dd7e8f612a6dd5ce04680
Rasmus Östersjö <rasmus.ostersjo@gmail.com>
2015-05-03 09:37:37 +0200

    Compressed tables

--- CardChallenge/Card.cpp
+++ CardChallenge/Card.cpp
@@ -4,30 +4,15 @@
 /** Table to print card colors
  */
 static const char* colorTable[COLOR + 1] {
-    "Hearts",
-    "Clubs",
-    "Spades",
-    "Diamonds",
+    "Hearts", "Clubs", "Spades", "Diamonds",
     "" /* No color */
 };
 
 /** Table ot print card values
  */
 static const char* valueTable[VALUE + 1] {
-    "Ace",
-    "Two",
-    "Three",
-    "Four",
-    "Five",
-    "Six",
-    "Seven",
-    "Eight",
-    "Nine",
-    "Ten",
-    "Jack",
-    "Queen",
-    "King",
-    "Joker",
+    "Ace", "Two",  "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine",
+    "Ten", "Jack", "Queen", "King", "Joker",
     "" /* No value */
 };
 
